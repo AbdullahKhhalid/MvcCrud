a6a7f1fd394d412ea53cf64cfb70d543de0501f5
Abdullah Khalid <abdullahkhhalid@gmail.com>
2023-05-14 15:47:17 +0500

    MvcCrud

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MvcCrud.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {}
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
